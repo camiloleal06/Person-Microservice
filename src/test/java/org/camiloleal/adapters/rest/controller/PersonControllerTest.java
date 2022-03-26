4cfa0d30a23446d28a8e5a3d97262a12a9b8654c
CAMILO LEAL <ingenierocamiloleal@gmail.com>
2022-03-26 14:54:01 -0500

    Test PersonService and Constrcutor in PersonEntity

--- src/test/java/org/camiloleal/adapters/rest/controller/PersonControllerTest.java
+++ src/test/java/org/camiloleal/adapters/rest/controller/PersonControllerTest.java
@@ -4,8 +4,6 @@ import org.camiloleal.adapters.bd.PersonEntity;
 import org.camiloleal.domain.model.Person;
 import org.camiloleal.domain.service.PersonServiceImpl;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mock;
-import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.Arrays;
 import java.util.List;
@@ -17,33 +15,39 @@ class PersonControllerTest {
 
     private final PersonServiceImpl personService = mock(PersonServiceImpl.class);
 
-    private final Person personDomain = mock(Person.class);
-
     private final PersonController personController = new PersonController(personService);
 
 
+    final int ID = 1;
+
+    PersonEntity person = PersonEntity.builder().build();
+
+    Person personDomain = person.toPerson();
+
+    Person personDomain2 = person.toPerson();
+
+    List<Person> listPerson = Arrays.asList(personDomain, personDomain2);
+
     @Test
     void shouldCallServiceCreate() {
-        //when(personService.create(personDomain)).thenReturn(personDomain);
+        when(personService.create(personDomain)).thenReturn(personDomain);
         personController.createPerson(personDomain);
         verify(personService, times(1)).create(personDomain);
     }
 
     @Test
     void shouldCallServiceFindAll() {
-     //   when(personService.findAll()).thenReturn(listPerson);
-     //   assertFalse(personController.findAllPeople().isEmpty());
-       // assertEquals(2, personController.findAllPeople().size());
-        personController.findAllPeople();
-        verify(personService, times(1)).findAll();
+        when(personService.findAll()).thenReturn(listPerson);
+        assertFalse(personController.findAllPeople().isEmpty());
+        assertEquals(2, personController.findAllPeople().size());
+        verify(personService, times(2)).findAll();
     }
 
     @Test
     void shouldCallServiceGetPersonById() {
-        //when(personService.getPersonById(ID)).thenReturn(personDomain);
-        //assertNotNull(personController.findPersonById(ID));
-        personController.findPersonById(anyInt());
-        verify(personService, times(1)).getPersonById(anyInt());
+        when(personService.getPersonById(ID)).thenReturn(personDomain);
+        assertNotNull(personController.findPersonById(ID));
+        verify(personService, times(1)).getPersonById(ID);
     }
 
 
