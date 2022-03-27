d9fb853332a45554257a1eb788b8c67b921d1b05
CAMILO LEAL <ingenierocamiloleal@gmail.com>
2022-03-27 12:39:37 -0500

    Refactoring Code PersonServiceImplTest

--- src/test/java/org/camiloleal/domain/service/PersonServiceImplTest.java
+++ src/test/java/org/camiloleal/domain/service/PersonServiceImplTest.java
@@ -1,35 +1,18 @@
 package org.camiloleal.domain.service;
 
-import org.camiloleal.adapters.bd.PersonEntity;
 import org.camiloleal.domain.model.Person;
 import org.junit.jupiter.api.Test;
 
-import java.util.Arrays;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.*;
-import static org.springframework.test.web.client.match.MockRestRequestMatchers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
-
-import org.junit.jupiter.api.extension.ExtendWith;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.TestPropertySource;
-import org.springframework.test.context.junit.jupiter.SpringExtension;
-import org.springframework.test.web.servlet.MockMvc;
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 
 class PersonServiceImplTest {
 
     private final PersonServiceImpl personService = mock(PersonServiceImpl.class);
 
     final int ID = 1;
-
-    PersonEntity person = PersonEntity.builder().build();
 
     Person personDomain = Person.builder().
             id(1)
@@ -42,18 +25,7 @@ class PersonServiceImplTest {
             .phone("73207639")
             .build();
 
-    Person personDomain2 = Person.builder().
-            id(1)
-            .dni("73207638")
-            .city("Cartagena")
-            .email("camiloleal3@gmail.com")
-            .firstName("camilo")
-            .lastName("Leal")
-            .old(30)
-            .phone("73207637")
-            .build();
-
-    List<Person> listPerson = Arrays.asList(personDomain, personDomain2);
+    List<Person> listPerson = List.of(personDomain);
 
     @Test
     void shouldCallServiceCreate() {
@@ -66,7 +38,7 @@ class PersonServiceImplTest {
     void shouldCallServiceFindAll() {
         when(personService.findAll()).thenReturn(listPerson);
         assertFalse(personService.findAll().isEmpty());
-        assertEquals(2, personService.findAll().size());
+        assertEquals(1, personService.findAll().size());
         verify(personService, times(2)).findAll();
     }
 
@@ -74,7 +46,7 @@ class PersonServiceImplTest {
     void shouldCallServiceGetPersonById() {
         when(personService.getPersonById(ID)).thenReturn(personDomain);
         assertNotEquals(null, personService.getPersonById(ID));
-        assertEquals("73207639",personService.getPersonById(ID).getDni());
+        assertEquals("73207639", personService.getPersonById(ID).getDni());
         verify(personService, times(2)).getPersonById(ID);
     }
 
