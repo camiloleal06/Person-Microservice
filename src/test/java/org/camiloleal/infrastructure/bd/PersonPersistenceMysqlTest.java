c02e7c912dc7ef84bc814e9dd0892f44655db408
CAMILO LEAL <ingenierocamiloleal@gmail.com>
2022-04-04 11:18:28 -0500

    Commit

--- src/test/java/org/camiloleal/infrastructure/bd/PersonPersistenceMysqlTest.java
+++ src/test/java/org/camiloleal/infrastructure/bd/PersonPersistenceMysqlTest.java
@@ -1,41 +1,52 @@
 package org.camiloleal.infrastructure.bd;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.camiloleal.domain.model.Person;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.annotation.Rollback;
-
-import static org.junit.jupiter.api.Assertions.*;
+import org.springframework.boot.test.mock.mockito.MockBean;
 
 @SpringBootTest
 class PersonPersistenceMysqlTest {
 
     @Autowired
+    PersonPersistenceMysql personPersistenceMysql;
+
+    @MockBean
     PersonRepository personRepository;
 
-    PersonEntity person = new PersonEntity();
+    PersonEntity personEntity = new PersonEntity(1, "73207639", "Camilo", "Leal", "ingenierocamiloleal@gmail.com",
+            "3225996394", 30, "Cartagena");
 
     @BeforeEach
     void setUp() {
-        person.setDni("73207333");
-        person.setCity("Cartagena");
-        person.setEmail("camiloleal2022@gmail.com");
-        person.setFirstName("camilo");
-        person.setLastName("Leal");
-        person.setOld(35);
-        person.setPhone("73207343");
+
+        personRepository = mock(PersonRepository.class);
+        personPersistenceMysql = new PersonPersistenceMysql(personRepository);
 
     }
 
     @Test
     void findAll() {
-        personRepository.findAll();
-        assertFalse(personRepository.findAll().isEmpty());
+        when(personRepository.findById(1)).thenReturn(Optional.of(personEntity));
+        Person person = personPersistenceMysql.getPersonById(1);
+        assertNotNull(person);
     }
+
     @Test
     void getPersonById() {
-        assertNotNull(personRepository.getById(1));
+        when(personRepository.findById(1)).thenReturn(Optional.of(personEntity));
+        Person person = personPersistenceMysql.getPersonById(1);
+        assertNotNull(person);
+        assertEquals("73207639", person.getDni());
     }
 
     @Test
@@ -43,11 +54,5 @@ class PersonPersistenceMysqlTest {
         assertNotNull(personRepository.findByEmail("camiloleal06@gmail.com"));
         assertEquals("Cartagena", personRepository.findByEmail("camiloleal06@gmail.com").getCity());
     }
-    @Test
-    @Rollback
-    void create() {
-        assertNotNull(personRepository.save(person));
-    }
-
 
 }
